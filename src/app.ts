98720d139d46773de0713e06208cbbf5ea099655
Jesus David Mejia <davidmejia-2000@outlook.com>
2024-02-24 14:59:58 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connected");
+
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    // console.log("Desde el Cliente", data);
+
+    const payload = JSON.stringify({
+      type: "custom-message",
+      payload: data.toString(),
+    });
+    // ws.send(JSON.stringify(payload));
+
+    //Todo - incluyente
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(payload, { binary: false });
+    //   }
+    // });
+
+    //Todo - excluyente
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
+  });
+
+  //   ws.send("Hola desde el servidor");
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log("Server running on port http://localhost:3000");
